1f552babdef3e96b953a0594568e88755283a15d
brezel-person <z.college42@gmail.com>
2026-07-09 03:21:56 +0000

    Finished making the quiz page functional, added some stylization changes

--- README.md
+++ README.md
@@ -7,8 +7,13 @@ What I've learned:
 - Learned how to use random number generation and take user input
 - Refreshed on operators
 - Learned more about using functions
+- Refreshed on iteration, re-learned how to do it in JS
+- Learned how to update the contents of a <div> based on the results of the quiz I made
 
 Challenges:
 - Figuring out how I want to make the memory game work
 - Figuring out how to make the quiz work with scores/points for the 3 different results
 - For some reason my font name suddenly changed, so I had to debug and figure out what had happened and why my font stopped showing up. Ended up re-linking it on the style.css page
+- I tried to add too many pages to my website, and realized I wouldn't be able to finish them all by the deadline. So, I decided to pivot and make one page less to make it on time.
+- accidentally deleted the "}" in one of my for-loops and had to debug it
+-Figuring out how to update the results on my quiz and how to go about iterating through the options & scores
--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const guideBtn = document.getElementById("guideBtn");
 const quizBtn = document.getElementById("quizBtn");
-const memBtn = document.getElementById("memBtn");
 const paperBtn = document.getElementById("paperBtn");
 const homeBtn = document.getElementById("homeBtn");
 
@@ -13,9 +12,6 @@ if(guideBtn){
     });
     quizBtn.addEventListener("click", function(){
         window.location.href="quiz.html";
-    });
-    memBtn.addEventListener("click", function(){
-        window.location.href="memory-game.html";
     });
     paperBtn.addEventListener("click", function(){
         window.location.href="apple-paper-scissors.html";
@@ -34,6 +30,29 @@ const q2 = document.getElementById("q2");
 const q3 = document.getElementById("q3");
 const result = document.getElementById("result");
 
+const appleResults = {
+    granny: {
+        name: "Granny Smith",
+        image: "apple-images/granny-smith.webp",
+        desc: "You have a kind, granny soul at heart. The Granny Smith apple appreciates your energy."
+    },
+    honey: {
+        name: "Honeycrisp",
+        image: "apple-images/honeycrisp.jpeg",
+        desc: "People think they know you, but deep down they don't. Because at heart, you're a sweet and crunchy Honeycrisp apple. Yum!"
+    },
+    pineapple: {
+        name: "Pineapple",
+        image: "apple-images/pineapple.jpg.webp",
+        desc: "67 you don't follow directions and instead of receiving an apple you got a pineapple. You're very chill tho"
+    }
+}
+
+let scores ={
+    granny: 0,
+    honey: 0,
+    pineapple: 0,
+};
 
 $(document).ready(function(){
     var appleQuizPopUp = new bootstrap.Modal($('#appleQuizPopUp')[0]);
@@ -46,19 +65,46 @@ $("#startBtn").click(function (){
 });
 
 $("#q1 button").click(function (){
+    let chosenApple = $(this).data("apple");
+    scores[chosenApple]++;
+    
     $("#q1").hide();
     $("#q2").show();
 });
 
 $("#q2 button").click(function (){
+    let chosenApple = $(this).data("apple");
+    scores[chosenApple]++;
+    
     $("#q2").hide();
     $("#q3").show();
 });
 
 $("#q3 button").click(function (){
+    let chosenApple = $(this).data("apple");
+    scores[chosenApple]++;
+    
     $("#q3").hide();
     $("#result").show();
+
+    showSpiritApple();
 });
+
+function showSpiritApple(){
+    let winner = "granny";
+    
+    for(let apple in scores){
+        if(scores[apple] > scores[winner]){
+            winner = apple;
+        }
+    }
+    
+    $("#spiritAppleName").text(appleResults[winner].name);
+    $("#spiritAppleImg").attr("src", appleResults[winner].image);
+    $("#spiritAppleDesc").text(appleResults[winner].desc);
+}
+
+
 
 
 
